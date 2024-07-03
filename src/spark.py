771342ebab2165f82ed57903114a3e4d8680141d
LRKZZ <sashulya1006@mail.ru>
2024-07-03 20:44:25 +0600

    added work with postgresql

--- src/spark.py
+++ src/spark.py
@@ -1,20 +1,52 @@
 import os
 from datetime import timedelta
 import asyncio
+import psycopg2
 from tinkoff.invest import CandleInterval, AsyncClient
 from tinkoff.invest.utils import now
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, DoubleType, LongType, TimestampType
-from pyspark.sql import Row
+from dotenv import load_dotenv
 
-token = "t.4-0QUkKqbdomKc3vNLAKQ4OdihFkqEcJvSjOmUwWsEwb4Kliu1PLMPKl2eGGBo9SfMbzNMNcBQqYIC5MbA13rQ"
+load_dotenv()
 
-async def fetch_data():
+password = os.getenv("PASSWORD_SQL")
+token = os.getenv("TOKEN")
+
+figi_list = ["BBG004731032", "BBG004731354", "BBG004730ZJ9", "BBG004730RP0", "BBG004S681W1", "BBG004730N88", "BBG00475KKY8", "BBG004S68473", "BBG0047315D0", "BBG004S68614"]  # Добавьте здесь ваши FIGI
+
+def get_figi_id(figi_number):
+    try:
+        conn = psycopg2.connect(
+            dbname="financial_db",
+            user="postgres",
+            password=password,
+            host="localhost",
+            port="5432"
+        )
+        cur = conn.cursor()
+
+        query = "SELECT figi_id FROM figi_numbers WHERE figi_number = %s;"
+        cur.execute(query, (figi_number,))
+        figi_id = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+        return figi_id
+
+    except Exception as e:
+        print(f"Error fetching figi_id: {e}")
+        return None
+
+async def fetch_data(figi):
     data = []
+    figi_id = get_figi_id(figi)
+    if figi_id is None:
+        print(f"figi_id not found for {figi}")
+        return data
+    
     async with AsyncClient(token) as client:
         async for candle in client.get_all_candles(
-            figi="BBG004730N88",
-            from_=now() - timedelta(minutes=10),
+            figi=figi,
+            from_=now() - timedelta(minutes=1),
             interval=CandleInterval.CANDLE_INTERVAL_1_MIN,
         ):
             data.append([
@@ -23,40 +55,58 @@ async def fetch_data():
                 candle.high.units + candle.high.nano / 1e9,
                 candle.low.units + candle.low.nano / 1e9,
                 candle.close.units + candle.close.nano / 1e9,
-                candle.volume
+                candle.volume,
+                figi_id
             ])
     return data
 
-async def stream_data(spark):
-    schema = StructType([
-        StructField("time", TimestampType(), True),
-        StructField("open", DoubleType(), True),
-        StructField("high", DoubleType(), True),
-        StructField("low", DoubleType(), True),
-        StructField("close", DoubleType(), True),
-        StructField("volume", LongType(), True)
-    ])
-    
-    count = 0
+def check_and_insert_data_to_db(data):
+    try:
+        conn = psycopg2.connect(
+            dbname="financial_db",
+            user="postgres",
+            password=password,
+            host="localhost",
+            port="5432"
+        )
+        cur = conn.cursor()
 
+        for record in data:
+            check_query = """
+            SELECT 1 FROM candles
+            WHERE time_of_candle = %s AND open_price = %s AND high_price = %s AND low_price = %s
+            AND close_price = %s AND volume = %s AND figi_id = %s;
+            """
+            cur.execute(check_query, record)
+            exists = cur.fetchone()
+            
+            if not exists:
+                insert_query = """
+                INSERT INTO candles (time_of_candle, open_price, high_price, low_price, close_price, volume, figi_id)
+                VALUES (%s, %s, %s, %s, %s, %s, %s);
+                """
+                cur.execute(insert_query, record)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    except Exception as e:
+        print(f"Error inserting data: {e}")
+
+async def stream_data():
     while True:
-        data = await fetch_data()
-        count += 1
-
-        new_df = spark.createDataFrame(data, schema)
-
-        print(f"Received batch number: {count}")
-        new_df.show()
-
+        tasks = [fetch_data(figi) for figi in figi_list]
+        results = await asyncio.gather(*tasks)
+        
+        for data in results:
+            if data:
+                check_and_insert_data_to_db(data)
+                
         await asyncio.sleep(60)
 
 async def main():
-    spark = SparkSession.builder \
-        .appName("Tinkoff Invest Data Streaming") \
-        .config("spark.local.dir", "C:/tmp/spark-temp") \
-        .getOrCreate()
-
-    await stream_data(spark)
+    await stream_data()
 
 if __name__ == "__main__":
     asyncio.run(main())
