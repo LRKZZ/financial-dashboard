cae085d1c9fe2f6c1b57423ea2fd8823af0c877b
Dmitry Leshchev <113301696+Lapamore@users.noreply.github.com>
2024-07-05 21:21:13 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,18 +1,20 @@
 import os
 from dotenv import load_dotenv
-import redis
 
 load_dotenv()
 
-token = os.getenv("TOKEN")
-password = os.getenv("PASSWORD_SQL")
+PASSWORD = os.getenv("PASSWORD_SQL")
+TOKEN = os.getenv("TOKEN")
 
-figi_list = [
-    "BBG004731032", "BBG004731354", "BBG004730ZJ9", 
-    "BBG004730RP0", "BBG004S681W1", "BBG004730N88", 
-    "BBG00475KKY8", "BBG004S68473", "BBG0047315D0", 
-    "BBG004S68614"
-]
-
-# Настройка клиента Redis
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+figi_list = {
+    1: "BBG004731032", 
+    2: "BBG004731354", 
+    3: "BBG004730ZJ9", 
+    4: "BBG004730RP0", 
+    5: "BBG004S681W1", 
+    6: "BBG004730N88", 
+    7: "BBG00475KKY8", 
+    8: "BBG004S68473", 
+    9: "BBG0047315D0", 
+    10: "BBG004S68614"
+}
