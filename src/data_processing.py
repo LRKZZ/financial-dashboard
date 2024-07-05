5cdbe4b76af8327531dc1c3292be4dde75890543
Dmitry <Lostghost23@yandex.ru>
2024-07-05 21:23:46 +0300

    Successful upload to Redis database

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,36 +1,21 @@
 from datetime import timedelta, datetime
 from tinkoff.invest import CandleInterval, AsyncClient
 from tinkoff.invest.utils import now
-import asyncio
-from config import TOKEN, figi_list, redis_client
+from config import TOKEN, figi_list
 import json
+import asyncio
+import redis
 
-def cache_candle_data(figi, data):
-    key = f"candle_data:{figi}"
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+
+def store_candle_data(figi_number, figi, data):
     for item in data:
-        item['time'] = item['time'].isoformat()  # Преобразование datetime в строку
-    redis_client.set(key, json.dumps(data), ex=60)  # Кэширование данных на 60 секунд
-    print(f"Cached data for {figi}: {json.dumps(data)}")
+        key = f"candle_data:{figi_number}_{item['time'].isoformat()}"
+        item['time'] = item['time'].isoformat()  
+        redis_client.set(key, json.dumps(item)) 
+        print(f"Stored data for {figi_number}: {json.dumps(item)}")
 
-def get_cached_candle_data(figi):
-    key = f"candle_data:{figi}"
-    cached_data = redis_client.get(key)
-    if cached_data:
-        data = json.loads(cached_data)
-        for item in data:
-            item['time'] = datetime.fromisoformat(item['time'])  # Преобразование строки обратно в datetime
-        print(f"Retrieved cached data for {figi}: {data}")
-        return data
-    print(f"No cached data found for {figi}")
-    return None
-
-async def fetch_data(figi):
-    # Проверка наличия данных в кэше
-    cached_data = get_cached_candle_data(figi)
-    if cached_data:
-        print(f"Using cached data for {figi}")
-        return cached_data
-
+async def fetch_data(figi_number, figi):
     data = []
     
     async with AsyncClient(TOKEN) as client:
@@ -46,20 +31,23 @@ async def fetch_data(figi):
                 "low": candle.low.units + candle.low.nano / 1e9,
                 "close": candle.close.units + candle.close.nano / 1e9,
                 "volume": candle.volume,
+                "figi_id": figi_number,
             })
     
     if not data:
-        # Handle case where no new data is available
         print(f"No new data for {figi}")
-    
-    # Кэширование данных
-    cache_candle_data(figi, data)
+    else:
+        store_candle_data(figi_number, figi, data)
     
     print(data)
     return data
 
 async def stream_data():
     while True:
-        tasks = [fetch_data(figi) for figi in figi_list]
+        tasks = [fetch_data(figi_number, figi) for figi_number, figi in figi_list.items()]
         await asyncio.gather(*tasks)
         await asyncio.sleep(60)
+
+# Запуск основной функции
+if __name__ == "__main__":
+    asyncio.run(stream_data())
