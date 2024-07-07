77166bbb9e5927152fa09e3b859950896f25170b
LRKZZ <sashulya1006@mail.ru>
2024-07-07 19:22:01 +0600

    edit logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TopList from './TopList';
 import ChartComponent from './ChartComponent';
-import './App.css';
 
 function App() {
     return (
         <Router>
-            <div className="app">
-                <Routes>
-                    <Route path="/" element={<TopList />} />
-                    <Route path="/company/:figi_id" element={<ChartComponent />} />
-                </Routes>
-            </div>
+            <Routes>
+                <Route path="/" element={<TopList />} />
+                <Route path="/chart/:figi_id" element={<ChartComponent />} />
+            </Routes>
         </Router>
     );
 }
