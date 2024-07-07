4fbc4e44c0661f1c2500d664a82329d0ce170709
LRKZZ <sashulya1006@mail.ru>
2024-07-07 19:20:51 +0600

    added changes every 1min

--- frontend/src/TopList.js
+++ frontend/src/TopList.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import './TopList.css';
+import { useNavigate } from 'react-router-dom';
 
 const TopList = () => {
     const [topVolume, setTopVolume] = useState([]);
     const [topGainers, setTopGainers] = useState([]);
     const [topLosers, setTopLosers] = useState([]);
     const [showAll, setShowAll] = useState({ volume: false, gainers: false, losers: false });
+
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchTopVolume = async () => {
             try {
                 const result = await axios.get('/api/top_volume');
-                console.log('Top Volume:', result.data);
                 setTopVolume(result.data);
             } catch (error) {
                 console.error('Error fetching top volume:', error);
@@ -23,7 +23,6 @@ const TopList = () => {
         const fetchTopGainers = async () => {
             try {
                 const result = await axios.get('/api/top_gainers');
-                console.log('Top Gainers:', result.data);
                 setTopGainers(result.data);
             } catch (error) {
                 console.error('Error fetching top gainers:', error);
@@ -32,7 +31,6 @@ const TopList = () => {
         const fetchTopLosers = async () => {
             try {
                 const result = await axios.get('/api/top_losers');
-                console.log('Top Losers:', result.data);
                 setTopLosers(result.data);
             } catch (error) {
                 console.error('Error fetching top losers:', error);
@@ -42,6 +40,14 @@ const TopList = () => {
         fetchTopVolume();
         fetchTopGainers();
         fetchTopLosers();
+
+        const intervalId = setInterval(() => {
+            fetchTopVolume();
+            fetchTopGainers();
+            fetchTopLosers();
+        }, 60000);
+
+        return () => clearInterval(intervalId);
     }, []);
 
     const handleShowAll = (type) => {
@@ -49,12 +55,21 @@ const TopList = () => {
     };
 
     const formatPercentage = (value) => {
-        console.log('Formatting percentage:', value);
         return value !== undefined && value !== null ? value.toFixed(4) : '0.0000';
     };
 
-    const handleCompanyClick = (figi_id) => {
-        navigate(`/company/${figi_id}`);
+    const renderListItems = (items) => {
+        return items.map((item, index) => (
+            <li key={index} className="list-item" onClick={() => navigate(`/chart/${item.figi_id}`)}>
+                <img src={`/company_logos/${item.figi_id}.png`} alt={`${item.company_name} logo`} className="company-logo1" />
+                <div className="company-info">
+                    {item.company_name}
+                    <div className={`percent-change ${item.percentage_change >= 0 ? 'positive' : 'negative'}`}>
+                        {formatPercentage(item.percentage_change)}%
+                    </div>
+                </div>
+            </li>
+        ));
     };
 
     return (
@@ -62,11 +77,7 @@ const TopList = () => {
             <div className="top-section">
                 <h2>Топ по обороту</h2>
                 <ul>
-                    {(showAll.volume ? topVolume : topVolume.slice(0, 3)).map((item, index) => (
-                        <li key={index} onClick={() => handleCompanyClick(item.figi_id)} className="company-item">
-                            {item.company_name} - {item.total_volume} ₽
-                        </li>
-                    ))}
+                    {renderListItems(showAll.volume ? topVolume : topVolume.slice(0, 3))}
                 </ul>
                 <button onClick={() => handleShowAll('volume')}>
                     {showAll.volume ? 'Скрыть' : 'Смотреть все'}
@@ -75,11 +86,7 @@ const TopList = () => {
             <div className="top-section">
                 <h2>Взлеты дня</h2>
                 <ul>
-                    {(showAll.gainers ? topGainers : topGainers.slice(0, 3)).map((item, index) => (
-                        <li key={index} style={{ color: 'green' }} onClick={() => handleCompanyClick(item.figi_id)} className="company-item">
-                            {item.company_name} - {formatPercentage(item.percentage_change)}%
-                        </li>
-                    ))}
+                    {renderListItems(showAll.gainers ? topGainers : topGainers.slice(0, 3))}
                 </ul>
                 <button onClick={() => handleShowAll('gainers')}>
                     {showAll.gainers ? 'Скрыть' : 'Смотреть все'}
@@ -88,11 +95,7 @@ const TopList = () => {
             <div className="top-section">
                 <h2>Падения дня</h2>
                 <ul>
-                    {(showAll.losers ? topLosers : topLosers.slice(0, 3)).map((item, index) => (
-                        <li key={index} style={{ color: 'red' }} onClick={() => handleCompanyClick(item.figi_id)} className="company-item">
-                            {item.company_name} - {formatPercentage(item.percentage_change)}%
-                        </li>
-                    ))}
+                    {renderListItems(showAll.losers ? topLosers : topLosers.slice(0, 3))}
                 </ul>
                 <button onClick={() => handleShowAll('losers')}>
                     {showAll.losers ? 'Скрыть' : 'Смотреть все'}
