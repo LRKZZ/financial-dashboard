3c782111591b36cf8f7246529c2c0fcf0dadb68b
LRKZZ <sashulya1006@mail.ru>
2024-07-08 22:57:14 +0600

    added new logic for view buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TopList from './TopList';
 import ChartComponent from './ChartComponent';
+import DetailedList from './DetailedList';
 
-function App() {
+const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<TopList />} />
                 <Route path="/chart/:figi_id" element={<ChartComponent />} />
+                <Route path="/detailed-list/:type" element={<DetailedList />} /> {/* Обновление пути */}
             </Routes>
         </Router>
     );
-}
+};
 
 export default App;
